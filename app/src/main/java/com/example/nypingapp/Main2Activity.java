7790325c7adfea968277692f8ec62c920e11bbe4
funkitman <sundeka@hotmail.com>
2019-04-30 18:54:26 +0200

    committ 2 en liten backup

--- app/src/main/java/com/example/nypingapp/Main2Activity.java
+++ app/src/main/java/com/example/nypingapp/Main2Activity.java
@@ -6,13 +6,26 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main2Activity extends AppCompatActivity {
+
+    public static String logFileString ="";
+    public TextView textStatusen;
+    public ScrollView scrollviewen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        /**
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,6 +37,61 @@ public class Main2Activity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+         */
+
+        ReadLog();
+        TextView TextViewLog = (TextView)findViewById(R.id.TextViewLog);
+        TextViewLog.setText(logFileString);
+
+        //for scrolling med smooth scroll + flick
+        textStatusen = (TextView) findViewById(R.id.TextViewLog);
+        scrollviewen = (ScrollView) findViewById(R.id.SCROLLER_ID2);
+
+        scrollToBottom();
+
     }
 
+
+
+    private void scrollToBottom() {
+        {
+            scrollviewen.post(new Runnable() {
+                public void run() {
+                    scrollviewen.smoothScrollTo(0, textStatusen.getBottom());
+                }
+            });
+        }
+    }
+    public void buttonClose(View v){
+
+        //closes down the second view
+        finish();
+
+    }
+
+    public void ReadLog() {
+        //reading text from file
+        try {
+            FileInputStream fileIn = openFileInput("logfile.txt");
+            InputStreamReader InputRead = new InputStreamReader(fileIn);
+
+            char[] inputBuffer = new char[256];
+            String s = "";
+            int charRead;
+
+            while ((charRead = InputRead.read(inputBuffer)) > 0) {
+                // char to string conversion
+                logFileString = String.copyValueOf(inputBuffer, 0, charRead);
+
+            }
+            InputRead.close();
+            //Toast.makeText(getBaseContext(), s, Toast.LENGTH_SHORT).show();
+
+
+        }
+
+        catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "Exception"+ "File read failed: " + e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
 }
--- app/src/main/java/com/example/nypingapp/MainActivity.java
+++ app/src/main/java/com/example/nypingapp/MainActivity.java
@@ -3,11 +3,27 @@ package com.example.nypingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static String ipaddressString = "";
+    public static String toLogFile ="";
+    public static double ping =0;
+    public static String pingString = "";
+    public static String currentDateandTime="";
+    public static FileOutputStream fileout;
+    public static OutputStreamWriter outputWriter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
